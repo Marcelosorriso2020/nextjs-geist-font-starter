4c485f85c1c03584613492c19c5f796b2274f0a8
Marcelosorriso2020 <sorriso.ms24@gmail.com>
2025-07-24 20:30:02 -0300

    Add .next/static/chunks/src_app_modulos_page_tsx_cbecea4d._.js 1753399801911

---
+++ .next/static/chunks/src_app_modulos_page_tsx_cbecea4d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d7588497._.js",
+  "static/chunks/src_7d5fc036._.js"
+],
+    source: "dynamic"
+});
