5afbd9cd39d1b7036395ff9715dc7da52c9e5f13
Marcelosorriso2020 <sorriso.ms24@gmail.com>
2025-07-24 20:29:59 -0300

    Add .next/static/chunks/src_app_aula-demo_page_tsx_cbecea4d._.js 1753399799319

---
+++ .next/static/chunks/src_app_aula-demo_page_tsx_cbecea4d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c607f1bb._.js",
+  "static/chunks/node_modules_158b4bcd._.js"
+],
+    source: "dynamic"
+});
